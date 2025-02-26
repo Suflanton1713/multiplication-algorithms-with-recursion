539c8fef46f164be28daf62f468eb622027a5b7e
Libardo Alejandro Quintero Gómez <quintero.libardo@correounivalle.edu.co>
2025-02-25 22:48:11 -0500

    Bugs on splitAlgorithm resolved

--- src/main/scala/Multiplicacion/Main.scala
+++ src/main/scala/Multiplicacion/Main.scala
@@ -2,7 +2,7 @@ package Multiplicacion
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val resultado = splitMultiply(22223,32343) // Probamos con 2567 * 3
+    val resultado = splitMultiply(17235,25675) // Probamos con 2567 * 3
     println(s"Resultado de la multiplicación: $resultado")
     println("hi")
   }
