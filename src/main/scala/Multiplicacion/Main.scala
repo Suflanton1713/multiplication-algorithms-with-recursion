b155fbbfed71bdc762f61938a7f988221d5343dd
Libardo Alejandro Quintero Gómez <quintero.libardo@correounivalle.edu.co>
2025-03-05 22:41:20 -0500

    Main deleted

--- src/main/scala/Multiplicacion/Main.scala
+++
@@ -1,9 +0,0 @@
-package Multiplicacion
-
-object Main {
-  def main(args: Array[String]): Unit = {
-    val resultado = splitMultiply(22223,32343) // Probamos con 2567 * 3
-    println(s"Resultado de la multiplicación: $resultado")
-    println("hi")
-  }
-}
